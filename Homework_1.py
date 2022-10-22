593f1ee5884918f15d9d12666b729e3c5c1d3b73
andrew071176 <113465010+andrew071176@users.noreply.github.com>
2022-10-22 14:56:49 +0300

    Add files via upload

---
+++ Homework_1.py
@@ -0,0 +1,31 @@
+#HOMEWORK 1
+# Excercise 1. Write a Python-script that displays the message “Hello world”.
+print ('Hello World\n')
+
+# Excercise 2. Rewrite the first script to display three any messages.
+print ('Hello Father')
+print ('Hello Mather')
+print ('Hello Everyone\n')
+
+# Excercise 3. Write a Python-script to reads values for the length and width of a
+# rectangle and returns the area of the rectangle.
+rect_width = float(input('Please, input rectangle width: '))
+rect_lenth = float(input('Please, input rectangle lenth: '))
+print ('The area of rectangle is: ', rect_width*rect_lenth, '\n')
+
+# Excercise 4. Write a program that requests the user to enter two numbers and
+# prints the sum, product, difference and quotient of the two numbers.
+number_1 = float(input('Please, input number 1: '))
+number_2 = float(input('Please, input number 2: '))
+print ('The sum of two numbers is: ', number_1 + number_2)
+print ('The product of two numbers is: ', number_1 * number_2)
+print ('The difference of two numbers is: ', number_1 - number_2)
+print ('The quotient of two numbers is: ', number_1/number_2, '\n')
+
+# Exercise 5. Write a program that reads in the radius of a circle and prints the
+# circle’s diameter, circumference and area. Use the constant value 3.14159 for π.
+# Do these calculations in output statements.
+circle_radius = float(input ('Please, input radius of circle: '))
+print ('The circles`s diameter is: ', circle_radius*2)
+print ('The circumference is: ', 2*3.14159*circle_radius)
+print ('The circle`s area is: ', 3.14159*circle_radius**2)
