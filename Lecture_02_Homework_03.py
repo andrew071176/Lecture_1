0122375ef2bf5cc0b3ec874bb8613531ca48bf96
Pavlov_Andrew <113465010+andrew071176@users.noreply.github.com>
2022-10-24 17:52:01 +0300

    Update Lecture_02_Homework_03.py

--- Lecture_02_Homework_03.py
+++ Lecture_02_Homework_03.py
@@ -3,7 +3,7 @@
 # number entered is negative.
 number = float(input('Please, input number: '))
 check = bool(number >= 0) or print (f'You`ve entered a negative number {number}\n')
-check = bool(number < 0) or print (f'You`ve entered a not negative number!!!\n')
+check = bool(number < 0) or print ('You`ve entered a not negative number!!!\n')
 
 # 2. Write a Python program to check if the value a is less than 20 or not.
 a = float(input('Please, input variable a: '))
@@ -12,13 +12,13 @@ check = bool(a < 20) or print ('Variable a is not less than 20\n')
 
 # 3. Write a Python program to check if a given number is Zero or Not.
 number = float(input('Please, input number: '))
-check = bool(number != 0) or print (f'You`ve entered a number Zero\n')
-check = bool(number == 0) or print (f'You`ve entered not Zero\n')
+check = bool(number != 0) or print ('You`ve entered a number Zero\n')
+check = bool(number == 0) or print ('You`ve entered not Zero\n')
 
 # 4. Write a Python program to check if a given number is Even or Odd.
 number = float(input('Please, input number: '))
-check = bool(number%2 != 0) or print (f'You`ve entered an Even number\n')
-check = bool(number%2 == 0) or print (f'You`ve entered an Odd number\n')
+check = bool(number%2 != 0) or print ('You`ve entered an Even number\n')
+check = bool(number%2 == 0) or print ('You`ve entered an Odd number\n')
 
 # 5. Write a Python program to find largest number among three numbers
 # entered by user.
