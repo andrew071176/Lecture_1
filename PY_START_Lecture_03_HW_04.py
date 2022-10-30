07766dc14bb72a31f282175c2c332c2431de00f5
Pavlov_Andrew <113465010+andrew071176@users.noreply.github.com>
2022-10-30 17:49:26 +0200

    Update PY_START_Lecture_03_HW_04.py

--- PY_START_Lecture_03_HW_04.py
+++ PY_START_Lecture_03_HW_04.py
@@ -4,6 +4,7 @@
 # На одному поверсі - 4 квартири. Напишіть програму, яка від користувача отримує номер квартири
 # та виводить для заданої квартири номер під'їзду, поверху та номер на поверсі.
 # # Якщо такої квартира немає в цьому будинку, то необхідно повідомити користувача про це.
+# Завдання має бути вирішено без if
 num_flat = int(input('Please, enter flat number: '))
 check_num_flat = (num_flat >= 1 and num_flat <= 144) or\
                  print ('There is no such flat number in this house')
@@ -25,9 +26,9 @@ num_floor_flat_4th = not check_num_flat or num_flat%4 or\
 # 2. Написати програму, яка буде повертати для заданого року кількість днів. Рік є високосним,
 # якщо він кратний 4, але не кратний 100, а також якщо він кратний 400.
 num_year = int(input('Please, enter number of the year: '))
-leap_year_days = not num_year%4 and num_year%100 and not num_year%400 and\
+if not num_year%4 and num_year%100 or not num_year%400:
     print('Quantity days in the year: 366', '\n')
-non_leap_year = not num_year%4 and num_year%100 and not num_year%400 or\
+else:
     print('Quantity days in the year: 365', '\n')
 
 # 3. Трикутник існує лише тоді, коли сума будь-яких двох сторін більше третьої.
@@ -35,7 +36,7 @@ non_leap_year = not num_year%4 and num_year%100 and not num_year%400 or\
 a = int(input('Please, enter triangle side a: '))
 b = int(input('Please, enter triangle side b: '))
 c = int(input('Please, enter triangle side c: '))
-check_for_exist = (a + b)> c and (a + c) > b and (b + c)> a and\
-                  print('Triangle exists')
-check_for_not_exist = ((a + b)> c and (a + c) > b and (b + c)> a) or\
-                  print('Triangle doesn`t exist')
+if (a + b)> c and (a + c) > b and (b + c)> a:
+    print('Triangle exists')
+else:
+    print('Triangle doesn`t exist')
