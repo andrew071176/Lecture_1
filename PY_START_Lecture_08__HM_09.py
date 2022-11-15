73bee3b68e19eca34450c3718dc7f42b93356a0d
Pavlov_Andrew <113465010+andrew071176@users.noreply.github.com>
2022-11-16 00:24:23 +0200

    Update PY_START_Lecture_08__HM_09.py

--- PY_START_Lecture_08__HM_09.py
+++ PY_START_Lecture_08__HM_09.py
@@ -31,30 +31,61 @@
 # > 123,34
 # > one hundred twenty three dollars thirty four cents
 def f5() -> str:
-    num = input('Please, enter sum in USD no more than 1 billion: ')
-    while num.isalpha() or float(num) > 10**10:
-        num = input('Please, enter correct sum in USD no more than 1 billion: ')
-    num = f'{float(num):.2f}'
+    # num = input('Please, enter sum in USD no more than 1 billion: ')
+    # while num.isalpha() or float(num) > 10**10:
+    #     num = input('Please, enter correct sum in USD no more than 1 billion: ')
+    # num = f'{float(num):.2f}'
 
-    dict_units = {0: '', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
-                  6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
-                  10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen',
-                  16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen',
-                  20: 'twenty', 30: 'thirty', 40: 'fourty', 50: 'fifty',
-                  60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety'}
+    dict_units = {'.': '', ' .0': 'zero', '0': '', '00': '', '1': 'one', '2': 'two', '3': 'three', '4': 'four', '5': 'five',
+                  '6': 'six', '7': 'seven', '8': 'eight', '9': 'nine',
+                  '10': 'ten', '11': 'eleven', '12': 'twelve', '13': 'thirteen', '14': 'fourteen', '15': 'fifteen',
+                  '16': 'sixteen', '17': 'seventeen', '18': 'eighteen', '19': 'nineteen',
+                  '20': 'twenty', '30': 'thirty', '40': 'fourty', '50': 'fifty',
+                  '60': 'sixty', '70': 'seventy', '80': 'eighty', '90': 'ninety'}
 
-    num_str = '{0:,}'.format(float(num)).replace(',', ' ').replace('.', ' ')    #digit division
+    num = 5100000.10
+    num_str = '{0:,}'.format(float(num)).replace(',', ' ').replace('.', ' ') #digit division
+    print (num_str)
     num_list = list(num_str.split(' '))                                         #list of digit divisions
     print (num_list)
 
-    print (dict_units[int(num_list[0])])
-    print (dict_units[int(num_list[1:])] if dict_units[int(num_list[1:] in dict_units else:
-    print (dict_units[int(num_list[1]*10)] + dict_units[int(num_list[2])
+    dict_division = {0: 'million(s)', 1: 'thousand(s)', 2: 'dollar(s)', 3: 'cent(s)'}
+    for division in range (0, 4):
+        if len(num_list[division]) == 3:
+            print (dict_units[num_list[division][0]], end = ' ')
+            if bool(num_list[division][0]):
+                print (' hundred(s)')
+            if int(num_list[division][1:]) in dict_units:
+                print(dict_units[num_list[division][1:]], end = ' ')
+            else:
+                print(dict_units[str(int(num_list[division][1] * 10))] +\
+                      ' ' +\
+                      dict_units[num_list[division][2]], end = ' ')
+        elif len(num_list[division]) == 2:
+            if int(num_list[division]) in dict_units:
+                print(dict_units[num_list[division]], end = ' ')
+            else:
+                print(dict_units[str(int(num_list[division][0]) * 10)] +\
+                      ' ' +\
+                      dict_units[num_list[division][1]], end = ' ')
+        else:
+            print (dict_units[num_list[division][0]], end = ' ')
+        print (dict_division[division], end = ' ')
+
+
+    # print (dict_units[int(num_list[1:])] if dict_units[int(num_list[1:] in dict_units else:
+    # print (dict_units[int(num_list[1]*10)] + dict_units[int(num_list[2])
+
+
 
 
 
 f5()
 
+
+# 6. Напишіть функцію, яка переводить ціле число з римського запису до десяткового.
+# Наприклад: XXII -> 22
+# Докладніше: https://en.wikipedia.org/wiki/Roman_numerals
 
 # 6. Напишіть функцію, яка переводить ціле число з римського запису до десяткового.
 # Наприклад: XXII -> 22
