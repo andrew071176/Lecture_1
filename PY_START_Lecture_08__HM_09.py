cae225c5ed92754c5997f340d4fa336dd654f953
Pavlov_Andrew <113465010+andrew071176@users.noreply.github.com>
2022-11-18 16:38:06 +0200

    Update PY_START_Lecture_08__HM_09.py

--- PY_START_Lecture_08__HM_09.py
+++ PY_START_Lecture_08__HM_09.py
@@ -89,7 +89,6 @@ def f6(num_roman: str) -> int:
                      'XL': ' 40 ', 'L': ' 50 ', 'XC': ' 90 ', 'C': ' 100 ',
                      'CD': ' 400 ', 'D': ' 500 ', 'CM': ' 900 ', 'M': ' 1000 '}
 
-    print (num_roman)
     num_arabic = num_roman
     for lenth in (2, 1):
         for key in dict_num_roman:
