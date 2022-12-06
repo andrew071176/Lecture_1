ddbfb26a56ec1ec9bcaf31b0a195997f0ee5df21
Pavlov_Andrew <113465010+andrew071176@users.noreply.github.com>
2022-12-06 16:53:43 +0200

    Update PY_PRO_Lecture_12_HW13.py

--- PY_PRO_Lecture_12_HW13.py
+++ PY_PRO_Lecture_12_HW13.py
@@ -17,7 +17,7 @@ class Student(Person):
         super().__init__(name, surname, id)
         self.group = group
 
-    def get_student_data_list(self):
+    def get_student_data_list(self) -> list:
         return [self.name, self.surname, self.id, self.group]
 
     def __str__(self):
@@ -29,8 +29,8 @@ class Student(Person):
 class Group:
     def __init__(self):
         student_01 = Student('Ivan', 'Ivanov', 1, 'Python')
-        student_02 = Student('Petr', 'Petrov', 2, 'Python')
-        student_03 = Student('Sergey', 'Sergeyev', 3, 'Python')
+        student_02 = Student('Petr', 'Ivanov', 2, 'Python')
+        student_03 = Student('Sergey', 'Ivanov', 3, 'Python')
         student_04 = Student('Pavel', 'Pavlov', 4, 'Python')
         student_05 = Student('Anton', 'Antonov', 5, 'Python')
         student_06 = Student('Yana', 'Yanovna', 6, 'Java')
@@ -42,52 +42,68 @@ class Group:
         self.students_list = [student_01, student_02, student_03, student_04, student_05,
                               student_06, student_07, student_08, student_09, student_10]
 
-    def add_student(self, student: Student):
+    #adding by id
+    def add_student(self, student: Student) -> str:
         self.student = student
-        for element in self.students_list:
-            if self.student.get_student_data_list()[1] == element.get_student_data_list()[1]:
-                return f"{'Student ('}"\
-                       f"{' '.join(map(str, (self.student.get_student_data_list())))}" \
-                       f"{') is already enrolled'}"
-        self.students_list.append(' '.join(map (str, self.student.get_student_data_list())))
-        return f"{'Student ('}" \
-               f"{' '.join(map(str, (self.student.get_student_data_list())))}" \
-               f"{') is successfully enrolled'}"
+        if len(self.students_list) < 10:
+            for element in self.students_list:
+                if self.student.get_student_data_list()[2] ==\
+                        element.get_student_data_list()[2]:
+                    return f"{'Student ('}"\
+                           f"{' '.join(map(str, (self.student.get_student_data_list())))}" \
+                           f"{') is already enrolled'}\n"
+            self.students_list.append(student)
+            return f"{'Student ('}" \
+                   f"{' '.join(map(str, (self.student.get_student_data_list())))}" \
+                   f"{') is successfully enrolled'}\n"
+        else:
+            return f"{'Student ('}" \
+                   f"{' '.join(map(str, (self.student.get_student_data_list())))}" \
+                   f"{') can`t be enrolled into list because list is fully enrolled'}\n"
 
-    def removal_student(self, student: Student):
+    #removal by id
+    def removal_student(self, student: Student) -> str:
         self.student = student
         for element in self.students_list:
-            if self.student.get_student_data_list()[1] == element.get_student_data_list()[1]:
+            if self.student.get_student_data_list()[2] ==\
+                    element.get_student_data_list()[2]:
                 self.students_list.remove(element)
                 return f"{'Student ('}"\
                        f"{' '.join(map(str, (self.student.get_student_data_list())))}" \
-                       f"{') is successfully removed'}"
+                       f"{') is successfully removed'}\n"
             else:
                 return f"{'Student ('}"\
                        f"{' '.join(map(str, (self.student.get_student_data_list())))}" \
-                       f"{') isn`t removed (not found)'}"
+                       f"{') isn`t removed (not found)'}\n"
 
-    def search_student(self, student: Student):
-        self.student = student
+    #searching by surname
+    def search_student(self, surname: str) -> str:
+        self.surname = surname
+        found_students = ''
         for element in self.students_list:
-            if self.student.get_student_data_list()[1] == element.get_student_data_list()[1]:
-                return f"{'Student ('}" \
-                       f"{' '.join(map(str, (self.student.get_student_data_list())))}" \
-                       f"{') is found in list'}"
-            else:
-                return f"{'Student ('}" \
-                       f"{' '.join(map(str, (self.student.get_student_data_list())))}" \
-                       f"{') isn`t found in list'}"
+            if element.get_student_data_list()[1] == self.surname:
+                found_students += f"{' '.join(map(str, (element.get_student_data_list())))}\n"
+        if len (found_students) > 0:
+            return f"{'Student(s) with surname '}"\
+                   f"{self.student.get_student_data_list()[1]}"\
+                   f"{' found in list:'}\n"\
+                   f"{found_students}\n"
+        else:
+            return f"{'Student(s) with surname '}"\
+                   f"{self.student.get_student_data_list()[1]}"\
+                   f"{' isn`t found in list:'}"\
+                   f"{found_students}\n"
 
     def __str__(self):
-        return '\nStudents list:\n' + '-'*30 + '\n' +\
-               '\n'.join(map(str, self.students_list))
+        return f"{'Students list:'}\n{'-'*30}\n" + \
+               '\n'.join(map(str, self.students_list)) +'\n'
 
 student_1 = Student('Ivan', 'Ivanov', 1, 'Python')
-student_11 = Student('Maksim', 'Masksimov', 11, 'C++')
+student_11 = Student('Maksim', 'Ivanov', 11, 'C++')
 
 group_1 = Group()
+print(group_1.add_student(student_1))
+print(group_1.removal_student(student_1))
 print(group_1.add_student(student_11))
-print(group_1.removal_student(student_11))
-print(group_1.search_student(student_11))
+print(group_1.search_student('Ivanov'))
 print (group_1)
