71cda58bdc34a8826d5d833f2cf8c41913376219
Pavlov_Andrew <113465010+andrew071176@users.noreply.github.com>
2023-02-02 23:55:24 +0200

    Update admin.py

--- PY_PRO_Lecture_24_HW23/admin.py
+++ PY_PRO_Lecture_24_HW23/admin.py
@@ -1,1 +1,31 @@
 
+from django.contrib import admin
+from .models import About, Who_us, Menu, Specials, Events
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ['title', 'desc', 'photo']
+    list_editable = ['title', 'desc', 'photo']
+
+@admin.register(Who_us)
+class Who_usAdmin(admin.ModelAdmin):
+    list_display = ['title', 'position', 'is_visible', 'desc', 'photo']
+    list_editable = ['title', 'position', 'is_visible', 'desc', 'photo']
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['title', 'position', 'is_visible', 'ingredients', 'desc',
+                    'price', 'photo', 'is_special']
+    list_editable = ['position', 'is_visible', 'price']
+
+@admin.register(Specials)
+class SpecialsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'position', 'is_visible', 'ingredients', 'desc',
+                    'photo', 'is_special']
+    list_editable = ['position', 'is_visible']
+
+@admin.register(Events)
+class EventsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'position', 'is_visible', 'desc',
+                    'price', 'photo']
+    list_editable = ['position', 'is_visible', 'price']
